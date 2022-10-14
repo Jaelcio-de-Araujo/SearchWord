5d7f3b4bada53209e2d009204ffe33a9a66a388d
Jaelcio Araujo <jaelcio.araujo@brlink.com.br>
2022-10-14 18:31:59 -0300

    Inserindo html

--- index.php
+++ index.php
@@ -5,5 +5,19 @@ use  Jdev\Cacaalavra\SearchWord;
 
 $searching = SearchWord::searchWord('Algodão');
 
-print_r($searching);
+?>
 
+<!doctype html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Buscar palavras</title>
+</head>
+<body>
+    <input type="text" placeholder="Digite uma palavra para buscar">
+    <input type="submit" value="Buscar">    
+</body>
+</html>
